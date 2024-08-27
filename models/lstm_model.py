73f0b3bb24c250a89b4dab9dfa569da0f90be340
Harsh <harsh.work8421@gmail.com>
2024-08-27 13:12:31 +0530

    lstm and random_forest models updated

--- models/lstm_model.py
+++ models/lstm_model.py
@@ -3,3 +3,17 @@ import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from tensorflow.keras.optimizers import Adam
+
+class LSTMModel:
+    def __int__(self):
+        self.model= Sequential([
+            LSTM(50, activation='relu', input_shape=(60,5), return_sequences=True),
+            LSTM(50, activation='relu'),
+            Dense(1)
+        ])
+        self.model.compile(optimizer=Adam(learning_rate=0.001), loss='mse')
+
+    def fit(self, X_train, y_train):
+        self.model.fit(X_train, y_train, epochs=50, batch_size=32, verbose=0)
+    def predict(self, X_test):
+        return self.model.predict(X_test)
--- models/random_forest_model.py
+++ models/random_forest_model.py
@@ -1,3 +1,11 @@
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
+
+class RandomForestModel:
+    def __init__(self):
+        self.model= RandomForestRegressor(n_estimators=100)
+
+    def fit(self, X_train,y_train):
+        X_train_2d = X_train.reshape(X_train.shape[0],-1)
+        self.model.fit(X_train_2d, y_train)
