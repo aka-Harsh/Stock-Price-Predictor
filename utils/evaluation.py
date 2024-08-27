82ff4729462390437e3ddfb0b3285cf4fefa7a8c
Harsh <harsh.work8421@gmail.com>
2024-08-28 01:12:28 +0530

    evaluation.py updated

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -1,2 +1,13 @@
 import numpy as np
 import pandas as pd
+
+def calculate_probabilities(predictions):
+    diff = np.diff(predictions)
+    rise_count = np.sum(diff > 0)
+    fall_count = np.sum(diff < 0)
+    total_count = len(diff)
+    
+    rise_prob = (rise_count / total_count) * 100
+    fall_prob = (fall_count / total_count) * 100
+    
+    return rise_prob, fall_prob
