e59fbd566feb12b2b938b771862d434e6863a00b
Harsh <harsh.work8421@gmail.com>
2024-08-27 17:53:00 +0530

    random forest, xgboost and data preprocessing updated

--- models/xgboost_model.py
+++ models/xgboost_model.py
@@ -1,3 +1,22 @@
 from xgboost import XGBRegressor
 import pandas as pd
 import numpy as np
+
+class XGBoostModel:
+    def __init__(self):
+        self.model = XGBRegressor(n_estimators=100, learning_rate=0.1)
+
+    def fit(self, X_train, y_train):
+        X_train_2d = X_train.reshape(X_train.shape[0], -1)
+        self.model.fit(X_train_2d, y_train)
+
+    def predict(self, X_test):
+        X_test_2d = X_test.reshape(X_test.shape[0], -1)
+        predictions = []
+        for i in range(X_test_2d.shape[0]):
+            pred = self.model.predict(X_test_2d[i:i+1])
+            predictions.append(pred[0])
+            if i < X_test_2d.shape[0] - 1:
+                X_test_2d[i+1, :-5] = X_test_2d[i, 5:]
+                X_test_2d[i+1, -5:] = pred
+        return np.array(predictions).reshape(-1, 1)
--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -2,3 +2,27 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+
+def preprocess_data(data):
+    features = ['Open', 'High', 'Low', 'Close', 'Volume']
+    
+    X = data[features].values
+    y = data['Close'].values.reshape(-1, 1)
+
+    scaler = MinMaxScaler()
+    X_scaled = scaler.fit_transform(X)
+    y_scaled = scaler.fit_transform(y)
+
+    return X_scaled, y_scaled, scaler
+
+def create_sequences(X, y, time_steps=60):
+    X_seq, y_seq = [], []
+    for i in range(len(X) - time_steps):
+        X_seq.append(X[i:i+time_steps])
+        y_seq.append(y[i+time_steps])
+    
+    X_seq, y_seq = np.array(X_seq), np.array(y_seq)
+    
+    X_train, X_test, y_train, y_test = train_test_split(X_seq, y_seq, test_size=0.2, random_state=42)
+    
+    return X_train, X_test, y_train, y_test
